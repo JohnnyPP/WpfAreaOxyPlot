da5aef21636d44ba4e91ab5585f6f61165b5a18a
JohnnyPP <johnnnyppp@gmail.com>
2012-11-19 22:48:58 +0100

    Initial release

---
+++ WpfAreaOxyPlot/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OxyPlot.Wpf;
+
+namespace WpfAreaOxyPlot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// http://oxyplot.codeplex.com/SourceControl/changeset/view/52946448ea08#Source%2fExamples%2fWPF%2fWpfExamples%2fExamples%2fAreaDemo%2fMainViewModel.cs
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var vm = new MainViewModel();
+            DataContext = vm;
+        }
+    }
+}
